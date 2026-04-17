c62241bc2ddd7a21d17049d7639aaca3e184c6d0
Vishnu-2-D <vishnu70103@gmail.com>
2026-04-17 10:41:08 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,44 +1,38 @@
 public class Test {
 
-  static void add(){
+    static void add() {
+        int a = 10;
+        int b = 20;
+        int result = a + b;
+        System.out.println("The sum is " + result);
+    }
 
-    int a=10;
-    int b=20;
-    int result = a+b;
-    System.out.println("The sum is " +result);
+    static void sub() {
+        int c = 20;
+        int d = 10;
+        int result1 = c - d;
+        System.out.println("The diff is " + result1);
+    }
+
+    static void multi() {
+        int x = 5;
+        int y = 8;
+        int result2 = x * y;
+        System.out.println("The multiplication result is: " + result2);
+    }
+
     
-  }
-  static void sub(){
+    static void div() {
+        double a = 10;
+        double b = 15;
+        double result3 = a / b;
+        System.out.println("The division result is: " + result3);
+    }
 
-    int c=20;
-    int d=10;
-    int result1 = c-d;
-    System.out.println("The diff is " +result1);
-    
-  }
-
-   static void multi(){
-
-    int x=5;
-    int y=8;
-    int result2 = x*y;
-    System.out.println("The multiplcation result is: " +result2);
-    
-  }
-  
-   static void multi(){
-
-    int a=10;
-    int b=15;
-    int result2 = a/b;
-    System.out.println("The division result is: " +result3);
-    
-  }
-
-  public static void main(String[] args)
-  {
-    add();
-    sub();
-    multi();
-  }
+    public static void main(String[] args) {
+        add();
+        sub();
+        multi();
+        div(); 
+    }
 }
